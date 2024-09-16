5d2132d868a7e7759b266ad23371bda9f17842c1
Mihir Makwana <mihirpmakwana786@gmail.com>
2024-09-16 20:45:29 +0530

    Sidebar Created

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Hello World</h1>
+    <App />
+    {/* <h1>Hello World</h1> */}
   </React.StrictMode>
 );
 
