5746fc6ff7116141b22a46d0e3e415c504754ba0
Mihir Makwana <mihirpmakwana786@gmail.com>
2024-09-25 20:25:37 +0530

    Basic structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
-// import './App.css';
+import './App.css';
+import chatgptLogo from './assets/chatgptLogo.svg';
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <div className="sidebar">
         <div className="upperSide">
           <div className="upperSideTop">
-            <img src="" alt="" className="logo" />
+            <img src={chatgptLogo} alt="ChatGPT Logo" className="logo" />
             <span className="brand">ChatGPT</span></div>
           <button className="midBtn"><img src="" alt="" className="addBtn" />New Chat</button>
 
